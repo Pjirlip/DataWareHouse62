cbcc07b0b530dc2a06d4a0f8e994fe2015dd64e7
Pjirlip <philipp@dippel.rocks>
2023-06-19 05:57:49 +0200

    Finished Script

---
+++ README.md
@@ -0,0 +1,61 @@
+# Data Warehouse - Aufgabvenbaltt 6 Aufgabe 2
+
+Ziel des Programms ist die zerlegung einer CSV in Dimensionen und Fakten wie im Aufgabenbaltt gefordert. 
+
+## Vorbereitung
+
+Das Programm verwendet zum parsen und formatieren von CSV Daten das npm modul "csv". Deswegen muss als erstes das Modul installiert werden mittels: 
+```bash
+ npm install
+```
+
+## Test
+Eine angepasste CSV liegt vor und kann mittels Befehl `npm run test` ausprobiert werden.
+
+
+## Installieren
+Um das Skript zu installieren einfach:
+```bash
+npm install
+npm link .
+```
+ausführen. Danach kann das Script von überall mittels: 
+
+```bash
+
+dwh_tables <Eingabedatei> <Anzahl Dimensionen>
+
+# Beispiel:
+
+dwh_tables input.csv 3
+```
+
+Danach befinden sich die Dimensionsdateien und eine angpasste Fakten-CSV im ausgeführten Path. 
+Die einzelnen Schritte werden bei der Ausführung auf der Standartausgabe protokolliert. 
+
+Z.B:
+```
+
+Orignal File before Transmutation:
+ [
+  [ 'Micky', 'Hose', 'Rot', '39', '10' ],
+  [ 'Donald', 'Hose', 'Grün', '78', '25' ],
+  [ 'Micky', 'Jacke', 'Blau', '99', '23' ]
+]
+
+
+Dimensions:
+ [
+  Map(2) { 'Micky' => 'Micky', 'Donald' => 'Donald' },
+  Map(2) { 'Hose' => 'Hose', 'Jacke' => 'Jacke' },
+  Map(3) { 'Rot' => 'Rot', 'Grün' => 'Grün', 'Blau' => 'Blau' }
+]
+
+
+Original File after Transmutation:
+ [
+  [ 1, 1, 1, '39', '10' ],
+  [ 2, 1, 2, '78', '25' ],
+  [ 1, 2, 3, '99', '23' ]
+]
+```
---
+++ dwh_tables.js
@@ -0,0 +1,67 @@
+#!/usr/bin/env node
+
+import fs                           from "fs/promises"
+import {parse, generate, stringify} from "csv/sync"
+import path                         from "path"
+
+const FILEPATH         = process.argv[2] 
+const COUNT_DIMENSIONS = parseInt(process.argv[3] || 0)
+
+const PARSER_OPTIONS = {
+    skip_empty_lines: true,
+    columns: false,
+    delimiter: "\t",
+}
+
+const WRITER_OPTIONS = {
+    delimiter: "\t"
+}
+
+const main = async () => {
+
+    if(!COUNT_DIMENSIONS || !FILEPATH) {
+        console.log(COUNT_DIMENSIONS, FILEPATH, process.argv[3])
+        console.log("Arguments missing");
+        return
+    }
+
+    let originalData = null
+    const dimensions = []
+
+    const file   = await fs.readFile(path.resolve(FILEPATH), "utf-8")
+    originalData = parse(file, PARSER_OPTIONS)
+
+    console.log("Orignal File before Transmutation: \n", originalData, "\n\n")
+
+    for (let i = 0; i < COUNT_DIMENSIONS; i++) {
+        dimensions.push(new Map())
+        
+        // Aufbau der Dimensionen. Benutze Maps als Datentyp statt Arrays um Duplikate später nicht rausfiltern zu müssen :-)
+        for (let j = 0; j < originalData.length; j++) {
+            if(!dimensions[i].has(originalData[j][i]))
+            dimensions[i].set(originalData[j][i], originalData[j][i])
+        }
+    }
+    console.log("Dimensions: \n", dimensions, "\n\n")
+    
+    // Transmutate original file Data with new Keys and write Dimension files
+    for (const [index, dim] of dimensions.entries()) {
+        const d = Array.from(dim.values())
+
+        originalData = originalData.map(row => {
+            row[index] = d.indexOf(row[index]) + 1
+            return row
+        })
+
+        fs.writeFile(`dim${index + 1}.csv`, stringify(d.map((row, index) => {
+            return [index + 1, row]
+        }), WRITER_OPTIONS))
+    }
+
+    console.log("Original File after Transmutation: \n", originalData)
+
+    fs.writeFile("out.csv", stringify(originalData, WRITER_OPTIONS))
+}
+
+
+main()
